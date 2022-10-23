a506a8b05808ebfaee225b3fdf3e353135d9b254
SCHLUR01 <ricardo.schluter@heineken.com>
2022-10-23 13:12:30 +0200

    updated

--- esphome/components/zehnder_whr_rf/zehnder_whr_rf.h
+++ esphome/components/zehnder_whr_rf/zehnder_whr_rf.h
@@ -1,16 +1,21 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/spi/spi.h"
+#include "component.h"
+#include "sensor.h"
+#include "spi.h"
 
 namespace esphome {
 namespace zehnder_whr_rf {
-class ZehnderWhrRF : public PollingComponent {
+class ZehnderWhrRF : public PollingComponent, public Sensor {
+ Sensor *pressure_sensor = new Sensor();
  public:
   ZehnderWhrRF() : PollingComponent(60000) {}
+
   void setup() override {}
-  void update() override { }
+  void update() override { 
+    int pressure =1000;
+    pressure_sensor->publish_state(pressure / 100.0);
+  }
 };
 }  // namespace zehnder_whr_rf
 }  // namespace esphome
